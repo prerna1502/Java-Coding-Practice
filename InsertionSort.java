a3651e45a898addb83e6e2b7d153b9b94772b3b4
Prerna Kumari <prer1506@gmail.com>
2022-08-01 19:25:11 +0530

    added

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,21 +1,23 @@
 public class InsertionSort {
-    public static void main(String[] args) {
-        int arr[]={7,8,3,1,2};
-        for(int i =0;i<arr.length;i++){
-            int smallest=i;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[smallest]>arr[j])
-                {
-                    smallest=j;
-                }
-            }
-            int temp=arr[smallest];
-            arr[smallest]=arr[i];
-            arr[i]=temp;
-        }
+    public static void printArray(int arr[]){
         for(int i=0;i<arr.length;i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
+        System.out.print(arr[i]+" ");
+        System.out.println();
     }
+    public static void main(String[] args) {
+        int arr[]={7,8,1,3,2};
+        for(int i=1;i<arr.length;i++)
+        {
+            int current = arr[i];
+            int j = i -1;
+            while(j>=0 && arr[j]>current)
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+        printArray(arr);
+    }
+    
 }
